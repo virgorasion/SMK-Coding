e93561606690d3c63e56d20093a40637db0ad68b
M Nur Fauzan W <fauzan.widyanto1@gmail.com>
2018-08-13 12:47:40 +0700

    Finish Project

---
+++ app/src/main/java/widyanto/fauzan/smkcodingapp/MainActivity.java
@@ -0,0 +1,43 @@
+package widyanto.fauzan.smkcodingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fourth);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
